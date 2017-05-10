f52687c275a455b9eafc5c550743d60172cbb424
Kris Moore <kris@ixsystems.com>
2017-05-10 17:28:33 -0400

    Add some more configuration for GitLab plugin usage

--- post_install.sh
+++ post_install.sh
@@ -10,8 +10,8 @@ sysrc -f /etc/rc.conf nginx_enable=YES
 service postgresql initdb
 service postgresql start
 
-USER="gitlab"
-DB="gitlab"
+USER="git"
+DB="gitlabhq_production"
 
 # Save the config values
 echo "$DB" > /root/dbname
@@ -20,18 +20,17 @@ export LC_ALL=C
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
 PASS=`cat /root/dbpassword`
 
-echo "Database Name: $DB"
-echo "Database User: $USER"
-echo "Database Password: $PASS"
-
 # create user git
-psql -d template1 -U pgsql -c "CREATE USER git CREATEDB SUPERUSER;"
+psql -d template1 -U pgsql -c "CREATE USER ${USER} CREATEDB SUPERUSER;"
 
 # Create the GitLab production database & grant all privileges on database
-psql -d template1 -U pgsql -c "CREATE DATABASE gitlabhq_production OWNER git;"
+psql -d template1 -U pgsql -c "CREATE DATABASE ${DB} OWNER ${USER};"
 
 # Connect as superuser to gitlab db and enable pg_trgm extension
-psql -U pgsql -d gitlabhq_production -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
+psql -U pgsql -d ${DB} -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
+
+# Set a password on the postgres account
+psql -U pgsql -c "ALTER USER pgsql WITH PASSWORD '${PASS}';"
 
 # Enable Redis socket
 echo 'unixsocket /var/run/redis/redis.sock' >> /usr/local/etc/redis.conf
@@ -39,11 +38,30 @@ echo 'unixsocket /var/run/redis/redis.sock' >> /usr/local/etc/redis.conf
 # Grant permission to the socket to all members of the redis group
 echo 'unixsocketperm 770' >> /usr/local/etc/redis.conf
 
-# Activate the changes to redis.conf
-service redis start
-
 # Add git user to redis group
+service redis start
 pw groupmod redis -m git
 
-service gitlab start
+# Fix missing symlink
+ln -s /home /usr/home
+
+# Set git users home to /home/git
+pw usermod git -d /home/git
+
+# Configure Git global settings for git user
+# 'autocrlf' is needed for the web editor
+git config --global core.autocrlf input
+
+# Disable 'git gc --auto' because GitLab already runs 'git gc' when needed
+git config --global gc.auto 0
+
+# Enable packfile bitmaps
+git config --global repack.writeBitmaps true
+
 service nginx start
+service gitlab start
+
+echo "Database Name: $DB"
+echo "Database User: $USER"
+echo "Database Password: $PASS"
+
