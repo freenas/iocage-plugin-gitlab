3c59f4b9ff6e6df1124307dc105848b516143f73
Martin Wilke <miwi@ixsystems.com>
2019-09-17 12:50:23 +0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -88,7 +88,7 @@ su -l git -c "cd /usr/local/www/gitlab-ce && echo "yes" | rake gitlab:setup RAIL
 su -l git -c "cd /usr/local/www/gitlab-ce && rake gettext:compile RAILS_ENV=production"
 
 #Workaround to fix fetch failers
-su -l git -c "cd /usr/local/www/gitlab-ce && rake yarn:install RAILS_ENV=production NODE_ENV=production"
+su -l git -c "cd /usr/local/www/gitlab-ce && rake yarn:install --har RAILS_ENV=production NODE_ENV=production"
 
 # Update node dependencies and recompile assets
 su -l git -c "cd /usr/local/www/gitlab-ce && rake yarn:install gitlab:assets:clean gitlab:assets:compile RAILS_ENV=production NODE_ENV=production"
